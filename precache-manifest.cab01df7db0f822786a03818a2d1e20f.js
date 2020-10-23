09ac80f9fd4c2c30ee8b0407bab8f4f13f309c52
piotrwrzodak <piotrwrzodakk@gmail.com>
2020-10-23 20:24:42 +0200

    Updates

---
+++ precache-manifest.cab01df7db0f822786a03818a2d1e20f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3987caee4294856a9072039c5b3eae1c",
+    "url": "/square-game/index.html"
+  },
+  {
+    "revision": "ec3eeb94c8618226bc95",
+    "url": "/square-game/static/css/main.d6180f92.chunk.css"
+  },
+  {
+    "revision": "72bccc3bcc25e5356439",
+    "url": "/square-game/static/js/2.953d225d.chunk.js"
+  },
+  {
+    "revision": "3c73c585782ac05880c0f89bcfdbba5a",
+    "url": "/square-game/static/js/2.953d225d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec3eeb94c8618226bc95",
+    "url": "/square-game/static/js/main.ef623a85.chunk.js"
+  },
+  {
+    "revision": "f6653d899f61c8051873",
+    "url": "/square-game/static/js/runtime-main.ea7fefb6.js"
+  }
+]);
