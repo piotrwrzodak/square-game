ce0c1ea65cdc2c695ffa4d6e1725bc61788f96af
piotrwrzodak <piotrwrzodakk@gmail.com>
2020-10-23 17:39:09 +0200

    implement lowerTheRest reducer responsible for updating grid after deletion of squares

--- src/features/grid/GridSquare.js
+++ src/features/grid/GridSquare.js
@@ -1,13 +1,18 @@
 import React from 'react'
+import { useDispatch } from 'react-redux';
+import { deleteSelected, lowerTheRest } from './gridSlice';
 import GridSearch from './gridSearch';
 
 
 export default function GridSquare(props) {
+  const dispatch = useDispatch(); 
   const classes = `grid-square color-${props.color}`
 
   const handleClick = () => {
     let squaresToDelete = GridSearch(props.id.toString().charAt(0), props.id.toString().charAt(1), props.array)
     console.log(squaresToDelete)
+    dispatch(deleteSelected(squaresToDelete));
+    dispatch(lowerTheRest(squaresToDelete));
   }
 
 
--- src/features/grid/gridSlice.js
+++ src/features/grid/gridSlice.js
@@ -26,18 +26,50 @@ export const gridSlice = createSlice({
     },
     reducers: {
       deleteSelected: (state, action) => {
+        // if in array is only one square do nothing
         if ( action.payload.length > 1) {
+          // replace every square from the array by 0
           for (let i = 0; i < action.payload.length; i++) {
             state.array[action.payload[i].slice(0,1)][action.payload[i].slice(1,2)] = 0
           }
         }
       },
       lowerTheRest: (state, action) => {
-        
-      },
-      generateNewOnes: (state, action) => {
+        // if in array is only one square do nothing
+        if ( action.payload.length > 1) {
+          // iterate on every square element  
+          for (let row = 4; row > -1; row--) {
+            for (let col = 0; col < 5; col++) {
+              // if current square is "deleted" go further
+              if (state.array[row][col] === 0) {
+                
+                // if current square row equals 0 => generate random square
+                if (row === 0) {
+                  state.array[row][col] = Math.floor(Math.random() * 6) + 1  
+                }
 
-      }
+                let i = 1
+    
+                while (state.array[row][col] === 0 ) {
+                  // if there is no square above generete new square
+                  if (row - i === -1) {
+                    state.array[row][col] = Math.floor(Math.random() * 6) + 1  
+                  }
+                  // if square at row (-i) above current square is deleted go higher
+                  if (row - i > -1 ? state.array[row - i][col] === 0  : false) {
+                    i++
+                  }
+                  // if square at row (-i) above current square is not deleted replace them
+                  if (row - i > -1 ? state.array[row - i][col] !== 0 : false) {
+                    state.array[row][col] = state.array[row - i][col]
+                    state.array[row - i][col] = 0
+                  }
+                }
+              }
+            }
+          }
+        }
+      },
     },
   });
 
@@ -45,6 +77,6 @@ export const gridSlice = createSlice({
 
   export const selectGrid = state => state.grid.array;
   
-  export const { deleteSelected, lowerTheRest, generateNewOnes} = gridSlice.actions;
-  
+  export const { deleteSelected, lowerTheRest} = gridSlice.actions;
+
   export default gridSlice.reducer;
